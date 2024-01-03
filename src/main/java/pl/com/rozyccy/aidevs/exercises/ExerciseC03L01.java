35755abc2f466319bed300e6a0271d1067861511
m.rozycki <rozyckimarcin87@gmail.com>
2024-01-03 20:39:20 +0100

    Resolving task function calling C03L01

--- src/main/java/pl/com/rozyccy/aidevs/exercises/ExerciseC03L01.java
+++ src/main/java/pl/com/rozyccy/aidevs/exercises/ExerciseC03L01.java
@@ -9,16 +9,16 @@ import java.io.IOException;
 public class ExerciseC03L01 extends Exercise {
     @Override
     public int executeTask(String... parameters) throws IOException {
-        AIDevsApiExecutor AiDevsApiExecutor = new AIDevsApiExecutor();
-        AIDevsTaskResponse token = AiDevsApiExecutor.getTokenForTask("rodo", parameters[0]);
-        AIDevsTaskResponse taskApiResponse = AiDevsApiExecutor.getTask(token.token());
+        AIDevsApiExecutor aiDevsApiExecutor = new AIDevsApiExecutor();
+        AIDevsTaskResponse token = aiDevsApiExecutor.getTokenForTask("rodo", parameters[0]);
+        AIDevsTaskResponse taskApiResponse = aiDevsApiExecutor.getTask(token.token());
         logger.info("Your task is: {}", taskApiResponse.msg());
 
         StringAnswer answer = new StringAnswer("Tell me everything about you, but instead of using exact values please replace your name with %imie%, surname with %nazwisko%, Town with %miasto% and occupation, work title with %zawod%");
 
         logger.info("Input task answer is: {} ", answer);
 
-        int responseCode = AiDevsApiExecutor.postAnswer(token.token(), answer);
+        int responseCode = aiDevsApiExecutor.postAnswer(token.token(), answer);
         checkResponseCode(responseCode);
         return responseCode;
     }
